723fe338575d8c7ab22fa959c1fe57c4973303e8
Akash-M1 <83898316+Akash-M1@users.noreply.github.com>
2022-10-04 18:44:42 +0530

    Final Submission

--- script.js
+++ script.js
@@ -9,10 +9,14 @@ function callWeather(){
         var temp=data.current.temp_c;
         var humidity=data.current.humidity;
         var wind=data.current.wind_kph;
+        var condition=data.current.condition.text;
+        var icon=data.current.condition.icon;
         document.getElementById("city").innerHTML=city;
         document.getElementById("temp").innerHTML=temp;
         document.getElementById("humid").innerHTML=humidity;
         document.getElementById("wind").innerHTML=wind;
+        document.getElementById("cond").innerHTML=condition;
+        document.getElementById("condimg").src=icon;
         console.log(data);
     }).catch((error)=>{
         alert("Invalid City Name");
